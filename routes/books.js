b08da99d2d8deaafd8b78f37a7bb428a6718462d
Kolawole Ogounchi <kolawoleogounchi@outlook.com>
2019-10-17 07:06:18 -0400

    book-detail & page-not-found views added

--- routes/books.js
+++ routes/books.js
@@ -22,9 +22,45 @@ router.get('/', asyncHandler(async (req, res) => {
   res.render('index', { books, title: "My Library" });
 }))
 
-// Get /books/new that creates a new book form
+// Get /books/new request that creates a new book form
 router.get('/new', (req, res) => {
   res.render("new-book", { title: "New Book" });
 });
+
+// Post /books/new request that posts a new book to the database
+router.post('/new', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books/" + book.id);
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      const bookData = {
+        pageTitle: "New Book", 
+        book: book.dataValues, 
+        errors: error.errors
+      }
+      res.render("new-book", bookData );
+    } else {
+      throw error;
+    }  
+  }
+}));
+
+// Get /books/:id request that shows book detail form
+router.get("/:id/edit", asyncHandler(async(req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    const bookData = {
+      pageTitle: "Book Detail", 
+      book: book.dataValues, 
+      errors: error.errors
+    }
+    res.render("book-detail", bookData);      
+  } else {
+    res.status(404).render("page-not-found", { title: "Page Not Found" });
+  }
+}));
 
 module.exports = router;
